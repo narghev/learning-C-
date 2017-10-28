3ef08f1c6183fd1340ad580163dce9d7c7ac5eb9
Narek Ghevandiani <n.ghevandiani@gmail.com>
2017-10-28 17:20:51 +0400

    insertion sort

--- sorting_algorithms/array_sort.cpp
+++ sorting_algorithms/array_sort.cpp
@@ -10,9 +10,16 @@ void bubble_sort(int array[], int length){
                 swap(array[i-1], array[i]);
 }
 
+void insertion_sort(int a[], int length)
+{
+    for (int pass = 1; pass < length; pass++)
+        for (int element = pass; element > 0 && a[element] < a[element - 1]; element--)
+            swap(a[element - 1], a[element]);
+}
+
 int main(){
     int array_to_sort[] = {5,4,3,2,1,0};
-    bubble_sort(array_to_sort, 6);
+    insertion_sort(array_to_sort, 6);
     
     cout<< "Sorted array: ";
     for (int i = 0; i < 6; i++)
